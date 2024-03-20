bb8f6f0741c65751ae2030948491083546281d6b
Soowagger <soowagger@naver.com>
2024-03-20 17:43:59 +0900

    회원제 게시판 마무리

--- BoardProject/src/edu/kh/jdbc/board/model/dao/BoardDAO.java
+++ BoardProject/src/edu/kh/jdbc/board/model/dao/BoardDAO.java
@@ -147,7 +147,124 @@ public class BoardDAO {
 		
 		return result;
 	}
+
 	
+	/** 게시글 수정 SQL 수행 DAO
+	 * @param conn
+	 * @param boardTitle
+	 * @param boardContent
+	 * @param boardNo
+	 * @return result
+	 */
+	public int updateBoard(Connection conn, String boardTitle, String boardContent, int boardNo) throws Exception {
+		
+		int result = 0;
+		
+		try {
+			String sql = prop.getProperty("updateBoard");
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardTitle);
+			pstmt.setString(2, boardContent);
+			pstmt.setInt(3, boardNo);
+			
+			result = pstmt.executeUpdate();
+			
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+	
+	
+
+	
+	/** 게시글 삭제 SQL 수행 DAO
+	 * @param conn
+	 * @param boardNo
+	 * @return result
+	 */
+	public int deleteBoard(Connection conn, int boardNo) throws Exception {
+		
+		int result = 0;
+		
+		try {
+			String sql = prop.getProperty("deleteBoard");
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, boardNo);
+			
+			result = pstmt.executeUpdate();
+			
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+	
+	/** 다음 게시글 번호 조회 SQL 수행 DAO
+	 * @param conn
+	 * @return boardNo
+	 */
+	public int nextBoardNo(Connection conn) throws Exception {
+		
+		int boardNo = 0;
+		
+		try {
+			String sql = prop.getProperty("nextBoardNo");
+			
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			if(rs.next()) {
+				boardNo = rs.getInt(1);
+			}
+			
+		} finally {
+			close(rs);
+			close(stmt);
+		}
+		
+		return boardNo;
+	}
+	
+	
+	/** 게시글 삽입 SQL 수행 DAO
+	 * @param conn
+	 * @param boardTitle
+	 * @param boardContent
+	 * @param memberNo
+	 * @param boardNo
+	 * @return
+	 */
+	public int insertBoard(Connection conn, String boardTitle, 
+			String boardContent, int memberNo, int boardNo) throws Exception {
+		
+		int result = 0;
+		
+		try {
+			String sql = prop.getProperty("insertBoard");
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, boardNo);
+			pstmt.setString(2, boardTitle);
+			pstmt.setString(3, boardContent);
+			pstmt.setInt(4, memberNo);
+			
+			result = pstmt.executeUpdate();
+			
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+
 	
 	
 	
